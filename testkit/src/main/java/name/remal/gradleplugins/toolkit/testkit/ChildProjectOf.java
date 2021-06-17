0d3d0844dcaa096ebba4921e99572cfa5b3275fe
Semyon Levin <levin.semen@gmail.com>
2021-06-17 17:34:30 +0300

    docs

--- testkit/src/main/java/name/remal/gradleplugins/toolkit/testkit/ChildProjectOf.java
+++ testkit/src/main/java/name/remal/gradleplugins/toolkit/testkit/ChildProjectOf.java
@@ -5,9 +5,18 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+/**
+ * <p><small>This annotation is supposed to be used only on parameters. {@link ElementType#FIELD} target is added to
+ * simplify Lombok's @{@link RequiredArgsConstructor} and @{@link AllArgsConstructor} annotations usage if
+ * <code>lombok.copyableAnnotations += {@link name.remal.gradleplugins.toolkit.testkit.ChildProjectOf}</code></small>
+ * is set in <code>lombokj.config</code> file.</p>
+ */
 @Target({PARAMETER, FIELD})
 @Retention(RUNTIME)
 @Documented
