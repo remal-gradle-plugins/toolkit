1a57d83239c9620806b678303571db81f438d613
Semyon Levin <levin.semen@gmail.com>
2022-07-08 16:34:25 -0700

    `LazyInitializer.of()`

--- src/main/java/name/remal/gradleplugins/toolkit/LazyInitializer.java
+++ src/main/java/name/remal/gradleplugins/toolkit/LazyInitializer.java
@@ -1,18 +1,36 @@
 package name.remal.gradleplugins.toolkit;
 
+import static javax.annotation.meta.When.UNKNOWN;
+
+import java.util.function.Supplier;
+import javax.annotation.Nonnull;
 import lombok.SneakyThrows;
 
 public abstract class LazyInitializer<T> {
 
+    public static <T> LazyInitializer<T> of(Supplier<T> supplier) {
+        return new LazyInitializer<T>() {
+            @Nonnull(when = UNKNOWN)
+            @Override
+            protected T create() {
+                return supplier.get();
+            }
+        };
+    }
+
+
+    @Nonnull(when = UNKNOWN)
     protected abstract T create() throws Throwable;
 
 
     private static final Object NO_INIT = new Object();
 
+    @Nonnull(when = UNKNOWN)
     @SuppressWarnings("unchecked")
     private volatile T object = (T) NO_INIT;
 
     @SneakyThrows
+    @Nonnull(when = UNKNOWN)
     public final T get() {
         if (object == NO_INIT) {
             synchronized (this) {
