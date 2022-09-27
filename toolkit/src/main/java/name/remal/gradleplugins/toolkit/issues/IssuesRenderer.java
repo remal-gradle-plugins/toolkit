90e10fbbeb26087736481470f9766bf6fbf12d19
Semyon Levin <levin.semen@gmail.com>
2022-09-26 20:57:51 -0700

    `IssuesRenderer.renderIssuesToPath()`

--- toolkit/src/main/java/name/remal/gradleplugins/toolkit/issues/IssuesRenderer.java
+++ toolkit/src/main/java/name/remal/gradleplugins/toolkit/issues/IssuesRenderer.java
@@ -1,10 +1,12 @@
 package name.remal.gradleplugins.toolkit.issues;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.write;
+import static name.remal.gradleplugins.toolkit.PathUtils.createParentDirectories;
+import static name.remal.gradleplugins.toolkit.PathUtils.normalizePath;
 
 import java.io.File;
+import java.nio.file.Path;
 import lombok.SneakyThrows;
 import lombok.val;
 
@@ -13,15 +15,15 @@ public interface IssuesRenderer {
     String renderIssues(Iterable<? extends Issue> issues);
 
     @SneakyThrows
-    default void renderIssuesToFile(Iterable<? extends Issue> issues, File file) {
-        file = file.getAbsoluteFile();
-        val parentFile = file.getParentFile();
-        if (parentFile != null && !parentFile.equals(file)) {
-            createDirectories(parentFile.toPath());
-        }
-
+    default void renderIssuesToPath(Iterable<? extends Issue> issues, Path path) {
         val content = renderIssues(issues);
-        write(file.toPath(), content.getBytes(UTF_8));
+        path = normalizePath(path);
+        createParentDirectories(path);
+        write(path, content.getBytes(UTF_8));
+    }
+
+    default void renderIssuesToFile(Iterable<? extends Issue> issues, File file) {
+        renderIssuesToPath(issues, file.toPath());
     }
 
 }
