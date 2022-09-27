5d35ffd423f7b2aa335d8d885ae5eeb7be3dd564
Semyon Levin <levin.semen@gmail.com>
2022-09-26 17:55:54 -0700

    `ObjectUtils.unwrapProviders()`

--- toolkit/src/main/java/name/remal/gradleplugins/toolkit/ObjectUtils.java
+++ toolkit/src/main/java/name/remal/gradleplugins/toolkit/ObjectUtils.java
@@ -5,13 +5,34 @@ import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
 import static lombok.AccessLevel.PRIVATE;
 
+import groovy.lang.Closure;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+import java.util.function.IntSupplier;
+import java.util.function.LongSupplier;
+import java.util.function.Supplier;
 import javax.annotation.Nullable;
+import kotlin.Lazy;
+import kotlin.reflect.KCallable;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.gradle.api.provider.Provider;
 import org.jetbrains.annotations.Contract;
 
 @NoArgsConstructor(access = PRIVATE)
@@ -20,6 +41,79 @@ public abstract class ObjectUtils {
     @Contract(value = "_->param1", pure = true)
     public static <T> T doNotInline(T object) {
         return object;
+    }
+
+
+    @Nullable
+    @SneakyThrows
+    @SuppressWarnings("java:S3776")
+    public static Object unwrapProviders(@Nullable Object object) {
+        if (object == null) {
+            return null;
+
+        } else if (object instanceof Closure) {
+            val typedObject = (Closure<?>) object;
+            if (typedObject.getMaximumNumberOfParameters() == 0 && isEmpty(typedObject.getParameterTypes())) {
+                return unwrapProviders(typedObject.call());
+            } else {
+                return object;
+            }
+
+        } else if (object instanceof Optional) {
+            return unwrapProviders(((Optional<?>) object).orElse(null));
+        } else if (object instanceof OptionalInt) {
+            val typedObject = (OptionalInt) object;
+            return typedObject.isPresent() ? typedObject.getAsInt() : null;
+        } else if (object instanceof OptionalLong) {
+            val typedObject = (OptionalLong) object;
+            return typedObject.isPresent() ? typedObject.getAsLong() : null;
+        } else if (object instanceof OptionalDouble) {
+            val typedObject = (OptionalDouble) object;
+            return typedObject.isPresent() ? typedObject.getAsDouble() : null;
+
+        } else if (object instanceof Callable) {
+            return unwrapProviders(((Callable<?>) object).call());
+        } else if (object instanceof Supplier) {
+            return unwrapProviders(((Supplier<?>) object).get());
+        } else if (object instanceof BooleanSupplier) {
+            return ((BooleanSupplier) object).getAsBoolean();
+        } else if (object instanceof IntSupplier) {
+            return ((IntSupplier) object).getAsInt();
+        } else if (object instanceof LongSupplier) {
+            return ((LongSupplier) object).getAsLong();
+        } else if (object instanceof DoubleSupplier) {
+            return ((DoubleSupplier) object).getAsDouble();
+
+        } else if (object instanceof Future) {
+            return unwrapProviders(((Future<?>) object).get());
+        } else if (object instanceof CompletionStage) {
+            return unwrapProviders(((CompletionStage<?>) object).toCompletableFuture().get());
+
+        } else if (object instanceof AtomicBoolean) {
+            return ((AtomicBoolean) object).get();
+        } else if (object instanceof AtomicInteger) {
+            return ((AtomicInteger) object).get();
+        } else if (object instanceof AtomicLong) {
+            return ((AtomicLong) object).get();
+        } else if (object instanceof AtomicReference) {
+            return unwrapProviders(((AtomicReference<?>) object).get());
+
+        } else if (object instanceof Lazy) {
+            return unwrapProviders(((Lazy<?>) object).getValue());
+        } else if (object instanceof KCallable) {
+            val typedObject = (KCallable<?>) object;
+            if (isEmpty(typedObject.getTypeParameters())) {
+                return unwrapProviders(typedObject.call());
+            } else {
+                return typedObject;
+            }
+
+        } else if (object instanceof Provider) {
+            return unwrapProviders(((Provider<?>) object).getOrNull());
+
+        } else {
+            return object;
+        }
     }
 
 
@@ -49,6 +143,11 @@ public abstract class ObjectUtils {
         return value == null || !value.isPresent();
     }
 
+    @Contract(value = "null->true", pure = true)
+    public static boolean isEmpty(@Nullable Object[] value) {
+        return value == null || value.length == 0;
+    }
+
 
     @Contract(value = "null->false", pure = true)
     public static boolean isNotEmpty(@Nullable CharSequence value) {
@@ -73,6 +172,11 @@ public abstract class ObjectUtils {
     @Contract(value = "null->false", pure = true)
     @SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "java:S2789"})
     public static boolean isNotEmpty(@Nullable Optional<?> value) {
+        return !isEmpty(value);
+    }
+
+    @Contract(value = "null->false", pure = true)
+    public static boolean isNotEmpty(@Nullable Object[] value) {
         return !isEmpty(value);
     }
 
