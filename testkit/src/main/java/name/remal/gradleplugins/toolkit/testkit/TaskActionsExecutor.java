e57f0a778851b65fdfe697c788a3b524a1b1c296
Semyon Levin <levin.semen@gmail.com>
2022-10-30 11:30:14 -0700

    [release-later] Gradle 7.6 compatibility

--- testkit/src/main/java/name/remal/gradleplugins/toolkit/testkit/TaskActionsExecutor.java
+++ testkit/src/main/java/name/remal/gradleplugins/toolkit/testkit/TaskActionsExecutor.java
@@ -1,20 +1,24 @@
 package name.remal.gradleplugins.toolkit.testkit;
 
 import static lombok.AccessLevel.PRIVATE;
+import static name.remal.gradleplugins.toolkit.reflection.MethodsInvoker.invokeMethod;
 
 import lombok.NoArgsConstructor;
 import lombok.val;
 import name.remal.gradleplugins.toolkit.ReliesOnInternalGradleApi;
 import org.gradle.api.Task;
 import org.gradle.api.internal.TaskInternal;
+import org.gradle.api.specs.Spec;
 
 @NoArgsConstructor(access = PRIVATE)
 public abstract class TaskActionsExecutor {
 
     @ReliesOnInternalGradleApi
+    @SuppressWarnings("unchecked")
     public static boolean executeOnlyIfSpecs(Task task) {
         val taskInternal = (TaskInternal) task;
-        return taskInternal.getOnlyIf().isSatisfiedBy(taskInternal);
+        val spec = invokeMethod(taskInternal, Spec.class, "getOnlyIf");
+        return spec.isSatisfiedBy(taskInternal);
     }
 
     public static void executeActions(Task task) {
