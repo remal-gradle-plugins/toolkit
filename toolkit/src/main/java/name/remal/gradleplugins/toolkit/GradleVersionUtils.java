4171ff7b315658f5ff6a7a98a65ad7764440c1cf
Semyon Levin <levin.semen@gmail.com>
2022-11-30 18:33:46 -0800

    `GradleVersionUtils`

---
+++ toolkit/src/main/java/name/remal/gradleplugins/toolkit/GradleVersionUtils.java
@@ -0,0 +1,62 @@
+package name.remal.gradleplugins.toolkit;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.NoArgsConstructor;
+import lombok.val;
+import org.gradle.util.GradleVersion;
+
+@NoArgsConstructor(access = PRIVATE)
+public abstract class GradleVersionUtils {
+
+    public static boolean isCurrentGradleVersionLessThan(GradleVersion version) {
+        return GradleVersion.current().compareTo(version) < 0;
+    }
+
+    public static boolean isCurrentGradleVersionLessThan(String versionString) {
+        val version = GradleVersion.version(versionString);
+        return isCurrentGradleVersionLessThan(version);
+    }
+
+
+    public static boolean isCurrentGradleVersionLessThanOrEqualTo(GradleVersion version) {
+        return GradleVersion.current().compareTo(version) <= 0;
+    }
+
+    public static boolean isCurrentGradleVersionLessThanOrEqualTo(String versionString) {
+        val version = GradleVersion.version(versionString);
+        return isCurrentGradleVersionLessThanOrEqualTo(version);
+    }
+
+
+    public static boolean isCurrentGradleVersionEqualTo(GradleVersion version) {
+        return GradleVersion.current().compareTo(version) == 0;
+    }
+
+    public static boolean isCurrentGradleVersionEqualTo(String versionString) {
+        val version = GradleVersion.version(versionString);
+        return isCurrentGradleVersionEqualTo(version);
+    }
+
+
+    public static boolean isCurrentGradleVersionGreaterThanOrEqualTo(GradleVersion version) {
+        return GradleVersion.current().compareTo(version) >= 0;
+    }
+
+    public static boolean isCurrentGradleVersionGreaterThanOrEqualTo(String versionString) {
+        val version = GradleVersion.version(versionString);
+        return isCurrentGradleVersionGreaterThanOrEqualTo(version);
+    }
+
+
+    public static boolean isCurrentGradleVersionGreaterThan(GradleVersion version) {
+        return GradleVersion.current().compareTo(version) > 0;
+    }
+
+    public static boolean isCurrentGradleVersionGreaterThan(String versionString) {
+        val version = GradleVersion.version(versionString);
+        return isCurrentGradleVersionGreaterThan(version);
+    }
+
+
+}
