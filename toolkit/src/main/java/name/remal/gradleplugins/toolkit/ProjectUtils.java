7075b8bd4b4486b086e4bcf56d79f977c890be61
Semyon Levin <levin.semen@gmail.com>
2022-12-12 13:42:45 -0800

    [release-later] Additional methods to `ProjectUtils`

--- toolkit/src/main/java/name/remal/gradleplugins/toolkit/ProjectUtils.java
+++ toolkit/src/main/java/name/remal/gradleplugins/toolkit/ProjectUtils.java
@@ -1,13 +1,22 @@
 package name.remal.gradleplugins.toolkit;
 
+import static java.util.Collections.emptyList;
 import static lombok.AccessLevel.PRIVATE;
 import static name.remal.gradleplugins.toolkit.PathUtils.normalizePath;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import lombok.NoArgsConstructor;
 import lombok.val;
 import org.gradle.api.Action;
 import org.gradle.api.Project;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTree;
 
 @NoArgsConstructor(access = PRIVATE)
 public abstract class ProjectUtils {
@@ -33,6 +42,72 @@ public abstract class ProjectUtils {
         } else {
             project.afterEvaluate(action);
         }
+    }
+
+
+    /**
+     * <p>Gradle configuration cache doesn't allow using {@link Project} at execution time.</p>
+     * <p>Usage:</p>
+     * <pre>{@code
+     * classpathFileTree(
+     *     files -> getObjectFactory().fileCollection().from(files),
+     *     getArchiveOperations()::zipTree,
+     *     classpathFiles
+     * )
+     * }</pre>
+     */
+    public static FileTree newClasspathFileTree(
+        Function<Iterable<File>, FileCollection> fileCollectionFactory,
+        Function<File, FileTree> zipTreeFactory,
+        Iterable<File> files
+    ) {
+        AtomicReference<FileTree> resultRef = new AtomicReference<>();
+        Consumer<FileTree> addToResult = fileTree -> {
+            FileTree result = resultRef.get();
+            if (result == null) {
+                result = fileTree;
+            } else {
+                result = result.plus(fileTree);
+            }
+            resultRef.set(result);
+        };
+
+        List<File> directories = new ArrayList<>();
+        Runnable addDirectoriesToResult = () -> {
+            if (directories.isEmpty()) {
+                return;
+            }
+
+            val fileTree = fileCollectionFactory.apply(new ArrayList<>(directories)).getAsFileTree();
+            directories.clear();
+            addToResult.accept(fileTree);
+        };
+
+        for (val file : files) {
+            if (file.isDirectory()) {
+                directories.add(file);
+            } else if (file.isFile()) {
+                addDirectoriesToResult.run();
+                addToResult.accept(zipTreeFactory.apply(file));
+            }
+        }
+
+        addDirectoriesToResult.run();
+
+        val result = resultRef.get();
+        if (result == null) {
+            return fileCollectionFactory.apply(emptyList()).getAsFileTree();
+        }
+
+        return result;
+    }
+
+    public static FileTree newClasspathFileTree(Project project, Iterable<File> files) {
+        return newClasspathFileTree(
+            project::files,
+            project::zipTree,
+            files
+        );
     }
 
 }
