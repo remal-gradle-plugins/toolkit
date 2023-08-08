09166777a44008666900869617109253f86c31f0
Semyon Levin <levin.semen@gmail.com>
2023-08-08 15:18:58 -0700

    [release-later] `test-external-plugins.gradle`

--- testkit/src/main/java/name/remal/gradle_plugins/toolkit/testkit/GradleDependencyVersions.java
+++ testkit/src/main/java/name/remal/gradle_plugins/toolkit/testkit/GradleDependencyVersions.java
@@ -1,22 +1,42 @@
 package name.remal.gradle_plugins.toolkit.testkit;
 
+import static java.lang.String.format;
 import static lombok.AccessLevel.PRIVATE;
 
 import java.util.Optional;
 import lombok.NoArgsConstructor;
+import lombok.val;
 import name.remal.gradle_plugins.toolkit.ObjectUtils;
 
 @NoArgsConstructor(access = PRIVATE)
 public abstract class GradleDependencyVersions {
 
-    private static final String CORRESPONDING_KOTLIN_VERSION_PROPERTY = "corresponding-kotlin.version";
-
     public static String getCorrespondingKotlinVersion() {
-        return Optional.ofNullable(System.getProperty(CORRESPONDING_KOTLIN_VERSION_PROPERTY))
-            .filter(ObjectUtils::isNotEmpty)
-            .orElseThrow(() -> new AssertionError(
-                CORRESPONDING_KOTLIN_VERSION_PROPERTY + " system property is not set or empty"
-            ));
+        val propertyName = "corresponding-kotlin.version";
+
+        return getNotEmptySystemProperty(propertyName)
+            .orElseThrow(() -> new AssertionError(format(
+                "%s system property is not set or empty",
+                propertyName
+            )));
+    }
+
+    public static String getExternalPluginToTestVersion(String pluginId) {
+        val propertyName = "external-plugin-version-" + pluginId;
+
+        return getNotEmptySystemProperty(propertyName)
+            .orElseThrow(() -> new AssertionError(format(
+                "%s system property is not set or empty."
+                    + " Make sure that `%s` plugin is added as a dependency to `externalPluginsToTest` configuration.",
+                propertyName,
+                pluginId
+            )));
+    }
+
+
+    private static Optional<String> getNotEmptySystemProperty(String name) {
+        return Optional.ofNullable(System.getProperty(name))
+            .filter(ObjectUtils::isNotEmpty);
     }
 
 }
