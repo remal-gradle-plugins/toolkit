05a22e3a46a7718df1b0baace4aefde3d6c446c1
Semyon Levin <levin.semen@gmail.com>
2023-12-01 10:50:26 -0800

    [release-later] `TextIssuesRenderer.description`

--- toolkit/src/main/java/name/remal/gradle_plugins/toolkit/issues/TextIssuesRenderer.java
+++ toolkit/src/main/java/name/remal/gradle_plugins/toolkit/issues/TextIssuesRenderer.java
@@ -1,6 +1,7 @@
 package name.remal.gradle_plugins.toolkit.issues;
 
 import static java.util.stream.Collectors.joining;
+import static lombok.AccessLevel.PRIVATE;
 import static name.remal.gradle_plugins.toolkit.StringUtils.indentString;
 import static name.remal.gradle_plugins.toolkit.StringUtils.normalizeString;
 import static name.remal.gradle_plugins.toolkit.issues.Utils.appendDelimiter;
@@ -9,14 +10,25 @@ import static name.remal.gradle_plugins.toolkit.issues.Utils.streamIssues;
 
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.With;
 import lombok.val;
 import name.remal.gradle_plugins.toolkit.ObjectUtils;
 import org.intellij.lang.annotations.Language;
 
+@NoArgsConstructor
+@Getter
+@With
+@AllArgsConstructor(access = PRIVATE)
 public class TextIssuesRenderer implements IssuesRenderer {
+
+    private boolean description = true;
 
     @Language("TEXT")
     @Override
+    @SuppressWarnings("java:S3776")
     public String renderIssues(Iterable<? extends Issue> issues) {
         return streamIssues(issues)
             .map(issue -> {
@@ -45,12 +57,14 @@ public class TextIssuesRenderer implements IssuesRenderer {
                     }
                 }
 
-                val descriptionText = messageToText(issue.getDescription());
-                if (!descriptionText.isEmpty()) {
-                    if (!messageText.isEmpty()) {
-                        sb.append('\n');
+                if (description) {
+                    val descriptionText = messageToText(issue.getDescription());
+                    if (!descriptionText.isEmpty()) {
+                        if (!messageText.isEmpty()) {
+                            sb.append('\n');
+                        }
+                        sb.append('\n').append(indentString(descriptionText));
                     }
-                    sb.append('\n').append(indentString(descriptionText));
                 }
 
                 return sb.toString();
