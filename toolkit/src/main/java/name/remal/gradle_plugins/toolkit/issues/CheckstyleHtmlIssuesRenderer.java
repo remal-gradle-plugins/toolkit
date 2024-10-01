9cf5915e840d403a9e56efdb2cfe99bef315e64e
Semyon Levin <levin.semen@gmail.com>
2024-09-30 17:28:20 -0700

    [release-later] `CheckstyleHtmlIssuesRenderer`: add tool name parameter

--- toolkit/src/main/java/name/remal/gradle_plugins/toolkit/issues/CheckstyleHtmlIssuesRenderer.java
+++ toolkit/src/main/java/name/remal/gradle_plugins/toolkit/issues/CheckstyleHtmlIssuesRenderer.java
@@ -20,6 +20,17 @@ public class CheckstyleHtmlIssuesRenderer implements IssuesRenderer {
 
     private static final CheckstyleXmlIssuesRenderer XML_RENDERER = new CheckstyleXmlIssuesRenderer();
 
+
+    private final String toolName;
+
+    public CheckstyleHtmlIssuesRenderer(String toolName) {
+        this.toolName = toolName;
+    }
+
+    public CheckstyleHtmlIssuesRenderer() {
+        this("");
+    }
+
     @Override
     @SneakyThrows
     public String renderIssues(Iterable<? extends Issue> issues) {
@@ -27,18 +38,18 @@ public class CheckstyleHtmlIssuesRenderer implements IssuesRenderer {
         try (val xsltInputStream = openInputStreamForUrl(xsltUrl)) {
             val xslt = new StreamSource(xsltInputStream, xsltUrl.toString());
 
-            val xmlContent = XML_RENDERER.renderIssues(issues);
-            val source = new StreamSource(new StringReader(xmlContent));
-
-            val outputWriter = new StringWriter();
-
             val factory = TransformerFactory.newInstance();
             tryToSetXmlSetting(factory, ACCESS_EXTERNAL_DTD, "");
             tryToSetXmlSetting(factory, ACCESS_EXTERNAL_STYLESHEET, "");
             tryToSetXmlSetting(factory, FEATURE_SECURE_PROCESSING, true);
 
             val transformer = factory.newTransformer(xslt);
+            transformer.setParameter("toolName", toolName);
             transformer.setParameter("gradleVersion", GradleVersion.current().getVersion());
+
+            val xmlContent = XML_RENDERER.renderIssues(issues);
+            val source = new StreamSource(new StringReader(xmlContent));
+            val outputWriter = new StringWriter();
             transformer.transform(source, new StreamResult(outputWriter));
 
             return outputWriter.toString();
--- toolkit/src/test/java/name/remal/gradle_plugins/toolkit/issues/CheckstyleHtmlIssuesRendererTest.java
+++ toolkit/src/test/java/name/remal/gradle_plugins/toolkit/issues/CheckstyleHtmlIssuesRendererTest.java
@@ -3,6 +3,7 @@ package name.remal.gradle_plugins.toolkit.issues;
 import static java.util.Collections.singletonList;
 import static name.remal.gradle_plugins.toolkit.issues.Issue.newIssueBuilder;
 import static name.remal.gradle_plugins.toolkit.issues.TextMessage.textMessageOf;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.io.File;
@@ -13,7 +14,9 @@ import org.junit.jupiter.api.Test;
 @TagXslt
 class CheckstyleHtmlIssuesRendererTest {
 
-    private final CheckstyleHtmlIssuesRenderer renderer = new CheckstyleHtmlIssuesRenderer();
+    private static final String TOOL_NAME = "asdiuhAZDXUHZADklhjcAd";
+
+    private final CheckstyleHtmlIssuesRenderer renderer = new CheckstyleHtmlIssuesRenderer(TOOL_NAME);
 
     @Test
     void does_not_throw_exception() {
@@ -22,6 +25,26 @@ class CheckstyleHtmlIssuesRendererTest {
             .message(textMessageOf("message"))
             .build();
         assertDoesNotThrow(() -> renderer.renderIssues(singletonList(issue)));
+    }
+
+    @Test
+    void does_not_include_word_checkstyle() {
+        val issue = newIssueBuilder()
+            .sourceFile(new File("source"))
+            .message(textMessageOf("message"))
+            .build();
+        val renderedContent = renderer.renderIssues(singletonList(issue));
+        assertThat(renderedContent).doesNotContainIgnoringCase("checkstyle");
+    }
+
+    @Test
+    void includes_tool_name() {
+        val issue = newIssueBuilder()
+            .sourceFile(new File("source"))
+            .message(textMessageOf("message"))
+            .build();
+        val renderedContent = renderer.renderIssues(singletonList(issue));
+        assertThat(renderedContent).contains(TOOL_NAME);
     }
 
 }
