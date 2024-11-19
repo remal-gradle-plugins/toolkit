c732f70e3b7a7a322ce97fee6459c8e09dd6aaaa
Semyon Levin <levin.semen@gmail.com>
2024-11-19 13:54:56 -0800

    [release-later] Improve `PropertiesUtils`

--- toolkit/src/main/java/name/remal/gradle_plugins/toolkit/PropertiesUtils.java
+++ toolkit/src/main/java/name/remal/gradle_plugins/toolkit/PropertiesUtils.java
@@ -1,22 +1,30 @@
 package name.remal.gradle_plugins.toolkit;
 
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.newInputStream;
 import static java.nio.file.Files.newOutputStream;
 import static lombok.AccessLevel.PRIVATE;
 import static name.remal.gradle_plugins.toolkit.PathUtils.createParentDirectories;
 import static name.remal.gradle_plugins.toolkit.PathUtils.normalizePath;
 import static name.remal.gradle_plugins.toolkit.UrlUtils.openInputStreamForUrl;
 
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FilterOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.Properties;
+import java.util.TreeMap;
+import javax.annotation.WillClose;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.val;
@@ -24,15 +32,16 @@ import lombok.val;
 @NoArgsConstructor(access = PRIVATE)
 public abstract class PropertiesUtils {
 
+    public static Properties loadProperties(File file) {
+        return loadProperties(file.toPath());
+    }
+
     @SneakyThrows
-    public static Properties loadProperties(URL url) {
-        val properties = new Properties();
-        try (val inputStream = openInputStreamForUrl(url)) {
-            try (val reader = new InputStreamReader(inputStream, UTF_8)) {
-                properties.load(reader);
-            }
+    public static Properties loadProperties(Path path) {
+        path = normalizePath(path);
+        try (val inputStream = newInputStream(path)) {
+            return loadProperties(inputStream);
         }
-        return properties;
     }
 
     @SneakyThrows
@@ -40,56 +49,90 @@ public abstract class PropertiesUtils {
         return loadProperties(uri.toURL());
     }
 
-    public static Properties loadProperties(Path path) {
-        return loadProperties(normalizePath(path).toUri());
-    }
-
-    public static Properties loadProperties(File file) {
-        return loadProperties(file.toPath());
-    }
-
-
     @SneakyThrows
-    public static void storeProperties(Properties properties, Path path) {
-        path = normalizePath(path);
-        createParentDirectories(path);
-        try (val outputStream = new StripFirstLineOutputStream(newOutputStream(path))) {
-            properties.store(outputStream, null);
+    public static Properties loadProperties(URL url) {
+        try (val inputStream = openInputStreamForUrl(url)) {
+            return loadProperties(inputStream);
         }
     }
+
+    @SneakyThrows
+    public static Properties loadProperties(@WillClose InputStream inputStream) {
+        try (val reader = new InputStreamReader(inputStream, UTF_8)) {
+            return loadProperties(reader);
+        }
+    }
+
+    @SneakyThrows
+    public static Properties loadProperties(@WillClose Reader reader) {
+        val properties = new Properties();
+        try (val br = reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader)) {
+            properties.load(br);
+        }
+        return properties;
+    }
+
 
     public static void storeProperties(Properties properties, File file) {
         storeProperties(properties, file.toPath());
     }
 
-    @SuppressWarnings("java:S4349")
-    private static class StripFirstLineOutputStream extends FilterOutputStream {
-
-        public StripFirstLineOutputStream(OutputStream out) {
-            super(new BufferedOutputStream(out));
+    @SneakyThrows
+    public static void storeProperties(Properties properties, Path path) {
+        path = normalizePath(path);
+        createParentDirectories(path);
+        try (val outputStream = newOutputStream(path)) {
+            storeProperties(properties, outputStream);
         }
+    }
 
-        private boolean firstLineSeparatorStarted = false;
-        private boolean firstLineSeen = false;
+    @SneakyThrows
+    public static void storeProperties(Properties properties, @WillClose OutputStream outputStream) {
+        try (val writer = new OutputStreamWriter(outputStream, ISO_8859_1)) {
+            storeProperties(properties, writer);
+        }
+    }
 
-        @Override
-        public void write(int b) throws IOException {
-            if (firstLineSeparatorStarted && !isLineSeparator(b)) {
-                firstLineSeen = true;
+    @SneakyThrows
+    public static void storeProperties(Properties properties, @WillClose Writer writer) {
+        try (val bw = writer instanceof BufferedWriter ? (BufferedWriter) writer : new BufferedWriter(writer)) {
+            val map = new TreeMap<String, String>();
+            properties.forEach((key, value) -> map.put(key.toString(), value.toString()));
+            for (val entry : map.entrySet()) {
+                writeEscaped(entry.getKey(), true, bw);
+                bw.append('=');
+                writeEscaped(entry.getValue(), false, bw);
+                bw.append('\n');
             }
+        }
+    }
 
-            if (firstLineSeen) {
-                super.write(b);
-
-            } else if (isLineSeparator(b)) {
-                firstLineSeparatorStarted = true;
+    private static void writeEscaped(String string, boolean escapeSpace, Writer writer) throws IOException {
+        for (int index = 0; index < string.length(); index++) {
+            val ch = string.charAt(index);
+            if (ch == '\\'
+                || ch == '='
+                || ch == ':'
+                || ch == '#'
+                || ch == '!'
+                || (ch == ' ' && escapeSpace)
+                || (ch == ' ' && index == 0)
+            ) {
+                writer.append('\\').append(ch);
+            } else if (ch == '\t') {
+                writer.append("\\t");
+            } else if (ch == '\n') {
+                writer.append("\\n");
+            } else if (ch == '\r') {
+                writer.append("\\r");
+            } else if (ch == '\f') {
+                writer.append("\\f");
+            } else if (ch <= 31 || 127 <= ch) {
+                writer.append("\\u").append(String.format("%04x", (int) ch).toUpperCase());
+            } else {
+                writer.append(ch);
             }
         }
-
-        private static boolean isLineSeparator(int b) {
-            return b == '\n' || b == '\r';
-        }
-
     }
 
 }
---
+++ toolkit/src/test/java/name/remal/gradle_plugins/toolkit/PropertiesUtilsTest.java
@@ -0,0 +1,72 @@
+package name.remal.gradle_plugins.toolkit;
+
+import static java.lang.Character.MAX_CODE_POINT;
+import static java.lang.Character.MIN_CODE_POINT;
+import static java.util.Objects.requireNonNull;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.stream.IntStream;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+class PropertiesUtilsTest {
+
+    @Test
+    void storedStringsAreEscapedCorrectly() throws Throwable {
+        val string = new StringBuilder();
+        string.append(' ');
+        IntStream.rangeClosed(MIN_CODE_POINT, MAX_CODE_POINT).forEach(codePoint -> {
+            for (val ch : Character.toChars(codePoint)) {
+                string.append(ch);
+            }
+        });
+
+        val properties = new Properties();
+        properties.setProperty(string.toString(), string.toString());
+
+        final byte[] bytes;
+        try (val out = new ByteArrayOutputStream()) {
+            PropertiesUtils.storeProperties(properties, out);
+            bytes = out.toByteArray();
+        }
+
+        val parsedProperties = new Properties();
+        parsedProperties.load(new ByteArrayInputStream(bytes));
+
+        assertThat(parsedProperties).isEqualTo(properties);
+    }
+
+    @Test
+    void storedKeysAreSorted() throws Throwable {
+        Properties properties = null;
+        int maxAttempts = 10_000;
+        for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+            val currentProperties = new Properties();
+            currentProperties.setProperty("a-1", "a");
+            currentProperties.setProperty("b-" + attempt, "b");
+            val keys = new ArrayList<>(currentProperties.keySet());
+            if (keys.get(1).equals("a-1")) {
+                properties = currentProperties;
+                break;
+            }
+
+            if (attempt >= maxAttempts) {
+                throw new AssertionError("Can't create properties with not sorted keys");
+            }
+        }
+
+        final String content;
+        try (val writer = new StringWriter()) {
+            PropertiesUtils.storeProperties(requireNonNull(properties), writer);
+            content = writer.toString().replace(" ", "");
+        }
+
+        assertThat(content).startsWith("a-1=");
+    }
+
+}
