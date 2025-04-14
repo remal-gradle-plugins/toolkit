a312fa408d4855a73f92bcb37eef1ce58e6cc2b4
Semyon Levin <levin.semen@gmail.com>
2025-04-14 13:33:56 +0400

    [release-later] Add scopes to `TestClasspath`

--- testkit/src/main/java/name/remal/gradle_plugins/toolkit/testkit/TestClasspath.java
+++ testkit/src/main/java/name/remal/gradle_plugins/toolkit/testkit/TestClasspath.java
@@ -1,56 +1,39 @@
 package name.remal.gradle_plugins.toolkit.testkit;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static java.lang.Boolean.parseBoolean;
 import static java.lang.String.format;
 import static java.util.function.Predicate.not;
 import static lombok.AccessLevel.PRIVATE;
-import static name.remal.gradle_plugins.toolkit.LazyProxy.asLazyMapProxy;
+import static name.remal.gradle_plugins.toolkit.ObjectUtils.defaultValue;
 import static name.remal.gradle_plugins.toolkit.ObjectUtils.isEmpty;
 import static name.remal.gradle_plugins.toolkit.PropertiesUtils.loadProperties;
 
 import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+import javax.annotation.Nullable;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public abstract class TestClasspath {
 
-    private static final Map<String, String> PROPERTIES = asLazyMapProxy(() -> {
-        var propertiesFilePathEnvName = "NAME_REMAL_GRADLE_PLUGINS_TEST_CLASSPATH_FILE";
-        var propertiesFilePath = System.getenv(propertiesFilePathEnvName);
-        if (isEmpty(propertiesFilePath)) {
-            throw new AssertionError(format(
-                "`%s` environment variable is not set or empty",
-                propertiesFilePathEnvName
-            ));
-        }
-
-        var propertiesFile = new File(propertiesFilePath);
-        var result = ImmutableMap.<String, String>builder();
-        loadProperties(propertiesFile).forEach((key, value) -> {
-            if (key != null && value != null) {
-                result.put(key.toString(), value.toString());
-            }
-        });
-        return result.buildKeepingLast();
-    });
-
-    private static String getNotEmptySystemProperty(String name) {
-        var value = PROPERTIES.get(name);
-        if (isEmpty(value)) {
-            throw new AssertionError(format("`%s` test classpath property is not set or empty", name));
-        }
-        return value;
+    public static String getTestClasspathLibraryVersion(String groupAndModuleNotation) {
+        return getTestClasspathLibraryVersion("", groupAndModuleNotation);
     }
 
-
-    public static String getTestClasspathLibraryVersion(String groupAndModuleNotation) {
-        var version = getNotEmptySystemProperty(format(
+    public static String getTestClasspathLibraryVersion(
+        String scope,
+        String groupAndModuleNotation
+    ) {
+        var version = getNotEmptyClasspathProperty(scope, format(
             "%s|version",
             groupAndModuleNotation
         ));
@@ -58,8 +41,16 @@ public abstract class TestClasspath {
         return version;
     }
 
+
     public static String getTestClasspathLibraryFullNotation(String groupAndModuleNotation) {
-        var fullNotation = getNotEmptySystemProperty(format(
+        return getTestClasspathLibraryFullNotation("", groupAndModuleNotation);
+    }
+
+    public static String getTestClasspathLibraryFullNotation(
+        String scope,
+        String groupAndModuleNotation
+    ) {
+        var fullNotation = getNotEmptyClasspathProperty(scope, format(
             "%s|full-notation",
             groupAndModuleNotation
         ));
@@ -67,8 +58,16 @@ public abstract class TestClasspath {
         return fullNotation;
     }
 
-    public static Collection<Path> getTestClasspathLibraryFilePaths(String groupAndModuleNotation) {
-        var pathsString = getNotEmptySystemProperty(format(
+
+    public static Set<Path> getTestClasspathLibraryFilePaths(String groupAndModuleNotation) {
+        return getTestClasspathLibraryFilePaths("", groupAndModuleNotation);
+    }
+
+    public static Set<Path> getTestClasspathLibraryFilePaths(
+        String scope,
+        String groupAndModuleNotation
+    ) {
+        var pathsString = getNotEmptyClasspathProperty(scope, format(
             "%s|paths",
             groupAndModuleNotation
         ));
@@ -77,6 +76,66 @@ public abstract class TestClasspath {
             .filter(not(String::isEmpty))
             .map(Paths::get)
             .collect(toImmutableSet());
+    }
+
+
+    public static Set<String> getTestClasspathFirstLevelLibraryNotations() {
+        return getTestClasspathFirstLevelLibraryNotations("");
+    }
+
+    public static Set<String> getTestClasspathFirstLevelLibraryNotations(String scope) {
+        var properties = getClasspathProperties(scope);
+        return properties.entrySet().stream()
+            .filter(entry -> entry.getKey().toString().endsWith(IS_FIRST_LEVEL_NOTATION_SUFFIX))
+            .filter(entry -> parseBoolean(entry.getValue().toString()))
+            .map(Entry::getKey)
+            .map(Object::toString)
+            .map(key -> key.substring(0, key.length() - IS_FIRST_LEVEL_NOTATION_SUFFIX.length()))
+            .collect(toImmutableSet());
+    }
+
+    private static final String IS_FIRST_LEVEL_NOTATION_SUFFIX = "|is-first-level";
+
+
+    private static String getNotEmptyClasspathProperty(String scope, String propertyName) {
+        var properties = getClasspathProperties(scope);
+        var value = properties.getProperty(propertyName);
+        if (isEmpty(value)) {
+            throw new AssertionError(format(
+                "Test classpath property for scope `%s` is not set or empty: `%s`",
+                scope,
+                propertyName
+            ));
+        }
+        return value;
+    }
+
+    private static Properties getClasspathProperties(String scope) {
+        return SCOPE_PROPERTIES_CACHE.getUnchecked(defaultValue(scope));
+    }
+
+    private static final LoadingCache<String, Properties> SCOPE_PROPERTIES_CACHE =
+        CacheBuilder.newBuilder().build(CacheLoader.from(scope -> {
+            var propertiesFilePathEnvName = getPropertiesFilePathEnvName(scope);
+            var propertiesFilePath = System.getenv(propertiesFilePathEnvName);
+            if (isEmpty(propertiesFilePath)) {
+                throw new AssertionError(format(
+                    "Environment variable is not set or empty: `%s`",
+                    propertiesFilePathEnvName
+                ));
+            }
+
+            var propertiesFile = new File(propertiesFilePath);
+            return loadProperties(propertiesFile);
+        }));
+
+    private static String getPropertiesFilePathEnvName(@Nullable String scope) {
+        if (scope == null || scope.isEmpty()) {
+            return "NAME_REMAL_GRADLE_PLUGINS_TEST_CLASSPATH_FILE";
+        }
+
+        scope = scope.toUpperCase().replaceAll("\\W", "_");
+        return "NAME_REMAL_GRADLE_PLUGINS_TEST_CLASSPATH_" + scope + "_FILE";
     }
 
 }
